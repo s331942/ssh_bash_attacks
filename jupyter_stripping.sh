8ee0fb86d3f61c3b9246d540dff0de8733787deb
s331942 <s331942@studenti.polito.it>
2024-03-09 17:26:54 +0100

    remove files in the .gitignore file

--- jupyter_stripping.sh
+++
@@ -1,7 +0,0 @@
-jq --indent 1 \
-    '
-    (.cells[] | select(has("outputs")) | .outputs) = []
-    | (.cells[] | select(has("execution_count")) | .execution_count) = null
-    | .metadata = {"language_info": {"name":"python", "pygments_lexer": "ipython3"}}
-    | .cells[].metadata = {}
-    ' section1_DataExploration_and_preprocessing.ipynb > section1_DataExploration_and_preprocessing_stripped.ipynb && mv section1_DataExploration_and_preprocessing_stripped.ipynb section1_DataExploration_and_preprocessing.ipynb
